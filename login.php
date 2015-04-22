ac0454b91e73dd5a70663c3b9e747182f1acd472
LessOgu <leslie0523@gwu.edu>
2015-04-21 22:14:41 -0400

    Local Login
    logs into SQL on my computer

--- login.php
+++ login.php
@@ -1,28 +1,33 @@
 
   <?php
-    $username = "leslie0523";
-    $password = "secret15";
+  // logs into SQL locally
+    $username = "root";
+    $password = "";
     $hostname = "localhost";
     $dbc = mysql_connect($hostname, $username, $password)
       or die('Connection Error: ' . mysql_error());
     echo "Connected to MySQL<br>";
 
-    mysql_select_db('yourusername', $dbc) or die('DB Selection Error' .mysql_error());
     
-    $Username = $_POST['Building_name'];
-    $Pass = $_POST['Building_name'];
+    mysql_select_db('createdb', $dbc) or die('DB Selection Error' .mysql_error());
     
+    $Username = $_POST['inputEmail'];
+    $Pass = $_POST['inputPassword'];    
     $query = "(
-      SELECT Username
-      FROM Username as U, Username as E, Pass as P, Pass as S
-      WHERE U.username = E.username
-        AND P.Pass = S.Pass)";
+      SELECT Email and Pass
+      FROM Contributor
+      WHERE Username = '".$Username."'
+        AND Pass ='".$Pass."')";
 
     $result = mysql_query($query, $dbc)
       or die('Query Error: ' . mysql_error());
+
+
+    if(!$result) var_dump("not correct");
 
     mysql_close($dbc);
 
     echo 'Thanks you for logging in.<br />';
     echo 'Welcome ' . $Username;
+    
   ?>

bfd02a7a0185ba585b138a08f05ef1466337a435
LessOgu <leslie0523@gwu.edu>
2015-04-22 16:05:34 -0400

    userEnterInfo Errors
    Fixed the issues that came with my sync last night

--- userEnterInfo.php
+++ userEnterInfo.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
@@ -8,43 +9,27 @@
 <body>
   <h2>Submit a WiFi Speed</h2>
 
-
   <?php
-<<<<<<< HEAD
   // logs into SQL locally
   // when connected online, will need to be matched to correct host server, username, and password
     $username = "root";
     $password = "";
     $hostname = "localhost";
-=======
 
-
-   
-
-
-    $username = "leslie0523";
-    $password = "secret15";
-    $hostname = "student.seas.gwu.edu";
->>>>>>> origin/master
     $dbc = mysql_connect($hostname, $username, $password)
       or die('Connection Error: ' . mysql_error());
     echo "Connected to MySQL<br>";
 
-    mysql_select_db('createdb', $dbc) or die('DB Selection Error' .mysql_error());
+    mysql_select_db('yourusername', $dbc) or die('DB Selection Error' .mysql_error());
     
-    $GPS_location 
-    
+    $GPS_location = /*TODO*/;
     $Building_name = $_POST['Building_name'];
     $Floor = $_POST['Floor'];
     $Room_number = $_POST['Room_number'];
     $Street_address = $_POST['Street_address'];
 
-<<<<<<< HEAD
-    $query = "INSERT INTO Location (/*TODO gps location*/, Building_name, Floor, Room_number, Street_address)
-              VALUES (/*".$GPS_location."',*/'".$Building_name."','".$Floor."', '".$Room_number."', '".$Street_address."')";
-=======
-    $query = "INSERT INTO Location (`GPS_location`,`Building_name`,`Floor`,`Room_number`,`Street_adress`) VALUES('.$GPS_location.','.$Building_name.','.$Room_number.','.$Street_address.')";
->>>>>>> origin/master
+    $query = "(INSERT INTO Location (/*TODO gps location*/, Building_name, Floor, Room_number, Street_address)
+              VALUES('.$GPS_location.','.$Building_name.','.$Room_number.','.$Street_address.')";
 
     $result = mysql_query($query, $dbc)
       or die('Query Error: ' . mysql_error());
@@ -56,8 +41,6 @@
     echo ' on floor ' . $Floor . '<br />';
     echo ' in room ' . $Room_number . '<br />';
     echo ', street: ' . $Street_address . '<br />';
-    echo ', GPS location: ' . $GPS_location . '<br />'; 
-    */
+    echo ', GPS location: ' . $GPS_location . '<br />';
+    
   ?>
-</body>
-</html>

46a5ec377464dc5931787e13a5d1c1e7dafeb9b7
LessOgu <leslie0523@gwu.edu>
2015-04-28 00:05:42 -0400

    changed variables
    edited enter info

--- userEnterInfo.php
+++ userEnterInfo.php
@@ -27,10 +27,10 @@
     //$value = trim( str_replace( array( "document.write('", "');"), '', $Speed));
     $min=0;
     $max=1000000;
-    int rand( $min,  $max)
+    $rand( $min,  $max);
 
     $query = "(INSERT INTO Instance (Wifi_speed, Instance_id, GPS_location)
-              VALUES(".$Wifi_speed.",".rand().",".$GPS_location.")";
+              VALUES(".$Wifi_speed.",".$rand.",".$GPS_location.")";
     $query2 = "(INSERT INTO Location (GPS_location, Building_name, Floor, Room_number, Street_adress)
               VALUES(".$Wifi_speed.",".$Building_name.",".$Floor.",".$Room_number.",".$Street_adress.")";
    /* <form method='post' id=myform>
@@ -46,15 +46,14 @@
       });
     </script>*/
 
-    $Speed = file_get_contents( 'wiFiCalc.js');
-
-    $query = "(INSERT INTO Location (Floor, Room_number, Wifi_speed)
-              VALUES(".$Floor.",".$Room_number.",".$Speed.")";
-
+    // $Speed = file_get_contents( 'wiFiCalc.js');
 
     $result = mysql_query($query, $dbc)
       or die('Query Error: ' . mysql_error());
 
+    $resul2 = mysql_query($query2, $dbc)
+      or die('Query Error: ' . mysql_error());
+      
     echo 'Thanks for submitting the form.<br />';
     echo 'You are in ' . $Building_name;
     echo ' on floor ' . $Floor . '<br />';
